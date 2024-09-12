088d7c752f9a79c96d07a8ddfa08914a854425fd
617998 <sinchuri.617998@swl.k12.oh.us>
2024-09-12 08:43:38 -0400

    part 3

--- src/YearlyMaintenanceCost.java
+++ src/YearlyMaintenanceCost.java
@@ -1,5 +1,33 @@
-public class Main {
+import java.util.Scanner;
+
+public class YearlyMaintenanceCost {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variables
+        double maintenanceCostWinter;
+        double maintenanceCostSpring;
+        double maintenanceCostSummer;
+        double maintenanceCostFall;
+        double yearlyMaintenanceCost = 0;
+
+        //output asking about maintenance cost, user inputs.
+        System.out.println("What is your maintenance cost in the winter?");
+        maintenanceCostWinter = scan.nextDouble();
+
+        System.out.println("What is your maintenance cost in the spring?");
+        maintenanceCostSpring = scan.nextDouble();
+
+        System.out.println("What is your maintenance cost in the summer?");
+        maintenanceCostSummer = scan.nextDouble();
+
+        System.out.println("What is your maintenance cost in the fall?");
+        maintenanceCostFall = scan.nextDouble();
+
+        //adding to figure out the yearly cost
+        yearlyMaintenanceCost = maintenanceCostWinter + maintenanceCostSpring +
+                maintenanceCostSummer + maintenanceCostFall;
+
+        //Output of the total cost which will be yearly cost
+        System.out.println("Your yearly maintenance cost will be $" + yearlyMaintenanceCost + ".");
     }
 }
